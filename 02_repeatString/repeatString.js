fbf166383d240a0e495edd74f2dd83e1c4c85e93
Hans <hans.matta@gmail.com>
2023-02-07 14:28:43 -0500

    complete exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let finalString = '';
+    if (num <0 ) {
+        return `ERROR`;
+    }
+    else {
+    for (let i = 1; i<=num; i++){
+        finalString += string; 
+    }
+    return finalString;
+    }   
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // split string into array of characters
+    let characterArray = Array.from(string);
+    // reverse array
+    let reversedArray = characterArray.reverse();
+    // create a variable string for the array
+    let finalString = '';
+    // for each char in the reversed Array, add the new character to it
+    for (character of reversedArray) {
+        finalString += character;
+    }
+    return finalString;
 };
+
+
+// apparently also string . reverse . join
 
 // Do not edit below this line
 module.exports = reverseString;
